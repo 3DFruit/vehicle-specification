b734e7a2c9f2d24f0a3f170e5df1e2f874d95f28
Chirkin Ivan <fruitin3d@yandex.ru>
2023-01-19 20:56:35 +0700

    feat: считывать параметры из аргументов, а не консоли

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,35 +4,22 @@ import org.example.vehicles.Vehicle;
 import org.example.vehicles.VehicleFactory;
 
 import java.util.InputMismatchException;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         VehicleFactory factory = new VehicleFactory();
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            printMenu();
+        for (String arg : args) {
             try {
-                int menuOption = scanner.nextInt();
+                int menuOption = Integer.parseInt(arg);
                 Vehicle vehicle = factory.createVehicle(menuOption);
                 if (vehicle == null) {
                     System.out.println("НЕИЗВЕСТНЫЙ ТИП ТРАНСПОРТА");
-                }
-                else {
+                } else {
                     System.out.println(vehicle);
                 }
-            }
-            catch (InputMismatchException exception) {
+            } catch (InputMismatchException exception) {
                 exception.printStackTrace();
             }
         }
-    }
-
-    private static void printMenu() {
-        System.out.println("Выберите тип транспорта");
-        System.out.println("\t0 - Мотоцикл");
-        System.out.println("\t1 - Самокат");
-        System.out.println("\t2 - Автомобиль");
-        System.out.println("\t3 - Автобус");
     }
 }
